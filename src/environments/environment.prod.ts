e33cd591a746b761ebc983d0076a30901ded9734
breno500as <breno500as@github.com>
2019-08-24 22:31:09 -0300

    versão

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/sca-api-gateway/'
+  apiUrl: 'http://localhost:8080/sca-api-gateway/v1/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/sca-api-gateway/',
+  apiUrl: 'http://localhost:8080/sca-api-gateway/v1/',
 };
 
 /*
