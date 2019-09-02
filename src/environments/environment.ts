5604982bfcba024ab5bcacc0be071358275b5bc7
breno500as <breno500as@github.com>
2019-09-02 09:02:41 -0300

    url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
  
     //apiUrl: 'http://localhost:8080/v1/sca-api-gateway/',
-    apiUrl: 'https://puc-sca-backend2.herokuapp.com/v1/sca-api-gateway/'
+    apiUrl: 'https://puc-sca-backend.herokuapp.com/v1/sca-api-gateway/'
 };
 
 /*
