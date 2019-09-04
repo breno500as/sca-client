c36e093dffa55a163d29854442612f959c927946
breno500as <breno500as@gmail.com>
2019-09-03 22:55:19 -0300

    enum

--- src/app/enum/nivelAlertaEnum.ts
+++ src/app/enum/nivelAlertaEnum.ts
@@ -2,5 +2,6 @@ export enum NivelAlertaEnum {
 
     NIVEL_1_BAIXO = 'Nível 1 - Baixo',
     NIVEL_2_MEDIO = 'Nível 2 - Médio',
-    NIVEL_3_ALTO = 'Nível 3 - Alto'
+    NIVEL_3_ALTO = 'Nível 3 - Alto',
+    NIVEL_4_ROMPIMENTO_IMINENTE = 'Nível 4 - Rompimento eminente'
 }
